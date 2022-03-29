fc35c9c865e81ded561cd0bbaa5ca151a6aa3425
WHR <msl0000023508@gmail.com>
2022-03-29 14:37:53 +0800

    Improve mkfs.nbt

--- mkfs.nbt.c
+++ mkfs.nbt.c
@@ -21,24 +21,55 @@
 */
 
 #include "nbt.h"
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+
+static void print_usage(const char *name) {
+	fprintf(stderr, "Usage: %s [-C {gzip|zlib}] [-L <root-node-name>] [-v] <path>\n", name);
+}
 
 int main(int argc, char **argv) {
-	if(argv[1] && strcmp(argv[1], "--") == 0) {
-		argc--;
-		argv[1] = argv[0];
-		argv++;
+	nbt_compression_strategy compression = STRAT_GZIP;
+	char *root_node_name = (char *)"";
+	int verbose = 0;
+	while(1) {
+		int c = getopt(argc, argv, "C:L:N:vh");
+		if(c == -1) break;
+		switch(c) {
+			case 'C':
+				if(strcmp(optarg, "gzip") == 0) compression = STRAT_GZIP;
+				else if(strcmp(optarg, "zlib") == 0) compression = STRAT_INFLATE;
+				else {
+					fprintf(stderr, "%s: Compression type %s is not supported\n",
+						argv[0], optarg);
+					return -1;
+				}
+				break;
+			case 'L':
+			case 'N':
+				root_node_name = optarg;
+				break;
+			case 'v':
+				verbose = 1;
+				break;
+			case 'h':
+				print_usage(argv[0]);
+				return 0;
+			case '?':
+				return -1;
+		}
 	}
-	if(argc != 2) {
-		fprintf(stderr, "Usage: %s <path>\n", argv[0]);
+	if(argc - optind != 1) {
+		print_usage(argv[0]);
 		return -1;
 	}
 
 	struct nbt_node root_node = {
 		.type = TAG_COMPOUND,
-		.name = "",
+		.name = root_node_name,
 		.payload.tag_compound = malloc(sizeof(struct nbt_list))
 	};
 	if(!root_node.payload.tag_compound) {
@@ -47,16 +78,27 @@ int main(int argc, char **argv) {
 	}
 	INIT_LIST_HEAD(&root_node.payload.tag_compound->entry);
 
-	FILE *f = fopen(argv[1], "wb");
+	FILE *f = fopen(argv[optind], "wb");
 	if(!f) {
-		perror(argv[1]);
+		perror(argv[optind]);
 		return 1;
 	}
 
-	nbt_status status = nbt_dump_file(&root_node, f, STRAT_INFLATE);
+	if(verbose) {
+		char *s = nbt_dump_ascii(&root_node);
+		if(s) {
+			fprintf(stderr, "%s: Writing following NBT tree into '%s':\n\n%s\n",
+				argv[0], argv[optind], s);
+			free(s);
+		} else {
+			fprintf(stderr, "%s: nbt_dump_ascii: %s\n", argv[0], nbt_error_to_string(errno));
+		}
+	}
+
+	nbt_status status = nbt_dump_file(&root_node, f, compression);
 	if(status != NBT_OK) {
 		fprintf(stderr, "%s: Failed to save NBT to %s, %s\n",
-			argv[0], argv[1], nbt_error_to_string(status));
+			argv[0], argv[optind], nbt_error_to_string(status));
 		return 1;
 	}
 
